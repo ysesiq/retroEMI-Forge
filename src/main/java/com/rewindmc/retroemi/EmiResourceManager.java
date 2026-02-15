9f9d5cd33e5cdb401f09af795889dfb31dd7969b
yessiq <2146174683@qq.com>
2026-02-15 18:08:49 +0800

    optimize EmiResourceManager

--- src/main/java/com/rewindmc/retroemi/EmiResourceManager.java
+++ src/main/java/com/rewindmc/retroemi/EmiResourceManager.java
@@ -1,22 +1,32 @@
 package com.rewindmc.retroemi;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.*;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
-import com.google.common.collect.Lists;
 import com.google.gson.Gson;
-
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+
 import cpw.mods.fml.common.registry.LanguageRegistry;
 import dev.emi.emi.EmiPort;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.resources.*;
+import net.minecraft.client.resources.FallbackResourceManager;
+import net.minecraft.client.resources.FileResourcePack;
+import net.minecraft.client.resources.IResource;
+import net.minecraft.client.resources.IResourceManager;
+import net.minecraft.client.resources.IResourceManagerReloadListener;
+import net.minecraft.client.resources.IResourcePack;
+import net.minecraft.client.resources.SimpleReloadableResourceManager;
 import net.minecraft.util.ResourceLocation;
 
 public class EmiResourceManager implements IResourceManagerReloadListener {
@@ -25,122 +35,89 @@ public class EmiResourceManager implements IResourceManagerReloadListener {
     @Override
     public void onResourceManagerReload(IResourceManager resourceManager) {
         Minecraft client = Minecraft.getMinecraft();
-        ArrayList<String> arraylist = Lists.newArrayList("en_us");
-
-        if (!"en_us".equals(client.gameSettings.language)) {
-            arraylist.add(client.gameSettings.language.toLowerCase(java.util.Locale.ROOT));
-        }
-
-        this.loadLocaleDataFiles(resourceManager, arraylist);
-    }
-
-    public synchronized void loadLocaleDataFiles(IResourceManager resourceManager, List<String> languages) {
-        for (String code : languages) {
-            ResourceLocation id = EmiPort.id("emi", "lang/" + code.toLowerCase(Locale.ROOT) + ".json");
+        for (ResourceLocation id : EmiPort.findResources(resourceManager, "lang", s -> s.endsWith(".json"))) {
+            if (!id.getResourceDomain().equals("emi")) {
+                continue;
+            }
             try {
-                this.loadLocaleData(resourceManager.getAllResources(id));
+                String code = id.getResourcePath().replace("lang/", "").replace(".json", "");
+                if (code.equals(client.gameSettings.language.toLowerCase(Locale.ROOT))) {
+                    this.loadLocaleData(resourceManager.getAllResources(id), client.gameSettings.language);
+                }
             } catch (IOException ignore) {
             }
         }
     }
 
-    private void loadLocaleData(List<IResource> resources) throws IOException {
+    private void loadLocaleData(List<IResource> resources, String code) throws IOException {
         Gson gson = new Gson();
-
         for (IResource resource : resources) {
             try (InputStream stream = resource.getInputStream(); InputStreamReader reader = new InputStreamReader(stream, StandardCharsets.UTF_8)) {
                 JsonObject jo = gson.fromJson(reader, JsonObject.class);
                 for (Map.Entry<String, JsonElement> entry : jo.entrySet()) {
-                    LanguageRegistry.instance().addStringLocalization(entry.getKey(), Minecraft.getMinecraft().gameSettings.language, entry.getValue().getAsString());
+                    LanguageRegistry.instance().addStringLocalization(entry.getKey(), code, entry.getValue().getAsString());
                 }
             }
         }
     }
 
-//    public static Map<String, String> getLanguageMap(net.minecraft.client.resources.Locale locale) {
-//        Field languageListField;
-//        try {
-//            languageListField = Locale.class.getDeclaredField("field_135032_a");
-//            languageListField.setAccessible(true);
-//
-//            @SuppressWarnings("unchecked")
-//            Map<String, String> map = (Map<String, String>) languageListField.get(locale);
-//            return map;
-//        } catch (NoSuchFieldException | IllegalAccessException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-
     public Map<ResourceLocation, IResource> findResources(IResourceManager manager, String startingPath, Predicate<ResourceLocation> allowedPathPredicate) {
         Map<ResourceLocation, IResource> result = new HashMap<>();
-        if (!(manager instanceof SimpleReloadableResourceManager srm)) return result;
+        if (!(manager instanceof SimpleReloadableResourceManager srm)) {
+            return result;
+        }
 
-        boolean direct = startingPath.contains(".");
+        boolean isDirect = startingPath.contains(".");
         int cut = startingPath.lastIndexOf('/');
-        String folder = direct ? (cut >= 0 ? startingPath.substring(0, cut) : "") : startingPath;
-        String fileName = direct ? (cut >= 0 ? startingPath.substring(cut + 1) : startingPath) : null;
+        String folder = startingPath;
 
-        for (var entry : srm.domainResourceManagers.entrySet()) {
-            String domain = ((Map.Entry<String, FallbackResourceManager>) entry).getKey();
+        if (isDirect) {
+            folder = (cut < 0) ? "" : startingPath.substring(0, cut);
+        }
+
+        for (Object entry : srm.domainResourceManagers.entrySet()) {
+            String namespace = ((Map.Entry<String, FallbackResourceManager>) entry).getKey();
             FallbackResourceManager frm = ((Map.Entry<String, FallbackResourceManager>) entry).getValue();
-            String assetPrefix = "assets/" + domain + "/";
-            String folderPrefix = folder.isEmpty() ? assetPrefix : assetPrefix + folder + "/";
+            String assetPrefix = "assets/" + namespace + "/";
 
             for (IResourcePack pack : (List<IResourcePack>) frm.resourcePacks) {
+                if (!(pack instanceof FileResourcePack frp)) {
+                    continue;
+                }
+                try (ZipFile zip = new ZipFile(frp.resourcePackFile)) {
+                    Enumeration<? extends ZipEntry> entries = zip.entries();
+                    String folderPrefix = folder.isEmpty() ? "" : folder + "/";
+                    while (entries.hasMoreElements()) {
+                        ZipEntry ze = entries.nextElement();
+                        if (ze.isDirectory()) {
+                            continue;
+                        }
 
-                if (pack instanceof FolderResourcePack frp) {
-                    File root = new File(frp.resourcePackFile, folderPrefix);
-                    if (!root.exists()) continue;
+                        String fullPath = ze.getName();
+                        if (!fullPath.startsWith(assetPrefix)) {
+                            continue;
+                        }
 
-                    LinkedList<File> stack = new LinkedList<>();
-                    stack.add(root);
+                        String rel = fullPath.substring(assetPrefix.length());
 
-                    String basePath = frp.resourcePackFile.getAbsolutePath().replace("\\", "/") + "/" + assetPrefix;
+                        boolean pathMatches = isDirect ? rel.equals(startingPath) : rel.startsWith(folderPrefix);
 
-                    while (!stack.isEmpty()) {
-                        File f = stack.removeFirst();
-                        File[] list = f.listFiles();
-                        if (list == null) continue;
+                        if (!pathMatches) {
+                            continue;
+                        }
 
-                        for (File child : list) {
-                            if (child.isDirectory()) {
-                                stack.add(child);
-                                continue;
-                            }
+                        ResourceLocation id = EmiPort.id(namespace, rel);
+                        if (!allowedPathPredicate.test(id)) {
+                            continue;
+                        }
 
-                            if (direct && !child.getName().equals(fileName)) continue;
-
-                            String rel = child.getAbsolutePath().replace("\\", "/").replace(basePath, "");
-                            ResourceLocation id = EmiPort.id(domain, rel);
-                            if (!allowedPathPredicate.test(id)) continue;
-
-                            try { result.put(id, manager.getResource(id)); } catch (IOException ignored) {}
+                        try {
+                            result.put(id, manager.getResource(id));
+                        } catch (IOException ignored) {
                         }
                     }
-                }
 
-                else if (pack instanceof FileResourcePack frp) {
-                    try (ZipFile zip = new ZipFile(frp.resourcePackFile)) {
-                        Enumeration<? extends ZipEntry> en = zip.entries();
-
-                        while (en.hasMoreElements()) {
-                            ZipEntry ze = en.nextElement();
-                            if (ze.isDirectory()) continue;
-
-                            String name = ze.getName();
-                            if (!name.startsWith(assetPrefix)) continue;
-
-                            String rel = name.substring(assetPrefix.length());
-                            if (direct ? !rel.equals(startingPath) : !rel.startsWith(folder + "/")) continue;
-
-                            ResourceLocation id = EmiPort.id(domain, rel);
-                            if (!allowedPathPredicate.test(id)) continue;
-
-                            try {
-                                result.put(id, manager.getResource(id));
-                            } catch (IOException ignored) {}
-                        }
-                    } catch (IOException ignored) {}
+                } catch (IOException ignored) {
                 }
             }
         }
