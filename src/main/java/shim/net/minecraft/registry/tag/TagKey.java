d086b0bd4f71b347701ac4714281e5672e594aa9
yessiq <2146174683@qq.com>
2026-03-07 22:40:15 +0800

    fix import java.util.Objects; from TagKey.java

--- src/main/java/shim/net/minecraft/registry/tag/TagKey.java
+++ src/main/java/shim/net/minecraft/registry/tag/TagKey.java
@@ -23,6 +23,7 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
