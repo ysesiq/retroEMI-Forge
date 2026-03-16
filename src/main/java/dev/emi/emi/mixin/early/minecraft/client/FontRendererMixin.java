f1db1e1c95c2a5dd913eddc8b52e941c7d860dd3
yessiq <2146174683@qq.com>
2026-03-16 23:59:41 +0800

    del FontRendererMixin.java :(

--- src/main/java/dev/emi/emi/mixin/early/minecraft/client/FontRendererMixin.java
+++ src/main/java/dev/emi/emi/mixin/early/minecraft/client/FontRendererMixin.java
@@ -1,37 +1,37 @@
-package dev.emi.emi.mixin.early.minecraft.client;
-
-import com.rewindmc.retroemi.REMIMixinHooks;
-import net.minecraft.client.gui.FontRenderer;
-import org.objectweb.asm.Opcodes;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Slice;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-@Mixin(value = FontRenderer.class, priority = 2000)
-public abstract class FontRendererMixin {
-
-    @Shadow protected byte[] glyphWidth;
-
-//    @ModifyVariable(
-//        method = "renderStringAtPos",
-//        at = @At(
-//            value = "JUMP",
-//            opcode = Opcodes.GOTO,
-//            ordinal = 0
-//        ),
-//        ordinal = 0 // i
-//    )
-//    private int customFontColor(int original, String text, boolean shadow) {
-//        return REMIMixinHooks.applyCustomFormatCodes((FontRenderer) (Object) this, text, shadow, original);
+//package dev.emi.emi.mixin.early.minecraft.client;
+//
+//import com.rewindmc.retroemi.REMIMixinHooks;
+//import net.minecraft.client.gui.FontRenderer;
+//import org.objectweb.asm.Opcodes;
+//import org.spongepowered.asm.mixin.Mixin;
+//import org.spongepowered.asm.mixin.Shadow;
+//import org.spongepowered.asm.mixin.injection.At;
+//import org.spongepowered.asm.mixin.injection.Inject;
+//import org.spongepowered.asm.mixin.injection.ModifyVariable;
+//import org.spongepowered.asm.mixin.injection.Slice;
+//import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+//
+//@Mixin(value = FontRenderer.class, priority = 2000)
+//public abstract class FontRendererMixin {
+//
+//    @Shadow protected byte[] glyphWidth;
+//
+////    @ModifyVariable(
+////        method = "renderStringAtPos",
+////        at = @At(
+////            value = "JUMP",
+////            opcode = Opcodes.GOTO,
+////            ordinal = 0
+////        ),
+////        ordinal = 0 // i
+////    )
+////    private int customFontColor(int original, String text, boolean shadow) {
+////        return REMIMixinHooks.applyCustomFormatCodes((FontRenderer) (Object) this, text, shadow, original);
+////    }
+//
+//    // fix vanilla bug
+//    @Inject(method = "readGlyphSizes", at = @At(value = "INVOKE", target = "Ljava/io/InputStream;read([B)I", shift = At.Shift.AFTER))
+//    private void fixBrackets(CallbackInfo ci) {
+//        this.glyphWidth['（'] = 127;
 //    }
-
-    // fix vanilla bug
-    @Inject(method = "readGlyphSizes", at = @At(value = "INVOKE", target = "Ljava/io/InputStream;read([B)I", shift = At.Shift.AFTER))
-    private void fixBrackets(CallbackInfo ci) {
-        this.glyphWidth['（'] = 127;
-    }
-}
+//}
